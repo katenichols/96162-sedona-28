40508a48d1d61e2ae4220159419f14c2a1b84a48
katenichols <kat-nik79@ya.ru>
2020-06-14 19:43:48 +0500

    Добавляет интерактивную карту

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ let arrivalDate = searchForm.querySelector("[name=arrival-date]");
 let departureDate = searchForm.querySelector("[name=departure-date]");
 let numberAdults = searchForm.querySelector("[name=number-of-adults]");
 let numberChildren = searchForm.querySelector("[name=number-of-children]");
+let modalMap = document.querySelector(".modal-map");
+let modalMapImg = document.querySelector(".modal-map-img");
+let modalMapiFrame = document.createElement("iframe");
 
 let isStorageSupport = true;
 let storage = "";
@@ -61,3 +64,16 @@ window.addEventListener("keydown", function (evt) {
         }
     }
 });
+
+modalMapImg.remove();
+modalMap.appendChild(modalMapiFrame);
+modalMapiFrame.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1674119.8949708052!2d-112.9370305085306!3d34.962487740912074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872da132f942b00d%3A0x5548c523fa6c8efd!2z0KHQtdC00L7QvdCwLCDQkNGA0LjQt9C-0L3QsCA4NjMzNiwg0KHQqNCQ!5e0!3m2!1sru!2sru!4v1592137564780!5m2!1sru!2sru";
+modalMapiFrame.width = "1200px";
+modalMapiFrame.height = "594px";
+modalMapiFrame.frameBorder = "0";
+modalMapiFrame.style="border:0;"
+modalMapiFrame.allowFullscreen = "";
+modalMapiFrame.ariaHidden = "false";
+
+// <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1674119.8949708052!2d-112.9370305085306!3d34.962487740912074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872da132f942b00d%3A0x5548c523fa6c8efd!2z0KHQtdC00L7QvdCwLCDQkNGA0LjQt9C-0L3QsCA4NjMzNiwg0KHQqNCQ!5e0!3m2!1sru!2sru!4v1592137564780!5m2!1sru!2sru"
+//  width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
